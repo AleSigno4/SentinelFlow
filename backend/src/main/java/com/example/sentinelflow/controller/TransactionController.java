fca669656f64a9b229aac6b9c12ea05d86147ce5
Alessandro Signori <alesignori4@gmail.com>
2026-04-14 15:04:28 +0200

    feat: add deletion and patching method for transactions

--- backend/src/main/java/com/example/sentinelflow/controller/TransactionController.java
+++ backend/src/main/java/com/example/sentinelflow/controller/TransactionController.java
@@ -1,13 +1,18 @@
 package com.example.sentinelflow.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sentinelflow.model.Transaction;
+import com.example.sentinelflow.model.TransactionStatus;
 import com.example.sentinelflow.repository.TransactionRepository;
+
 
 
 
@@ -31,4 +36,16 @@ public class TransactionController {
         return entity;
     }
     
+    @DeleteMapping("/{id}")
+    public void deleteTransaction(@PathVariable Long id) {
+        this.transactionRepository.deleteById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Transaction patchTransaction(@PathVariable Long id, @RequestBody TransactionStatus status) {
+        Transaction transaction = this.transactionRepository.findById(id).orElseThrow();
+        transaction.setStatus(status);
+        this.transactionRepository.save(transaction);
+        return transaction;
+    }
 }
